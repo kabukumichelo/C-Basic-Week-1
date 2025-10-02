2ee64276a8091b66cf3e26f03acd841c41c368ef
kabukumichelo <michelo777690334@gmail.com>
2025-10-02 12:20:04 +0200

    error fixing

---
+++ Day2/project6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int num;
+    printf("please enter your number\n:");
+    scanf("%d", &num);
+    if (num % 2 == 0)
+    {printf("%d  is even", num);
+    } else {printf(" %d  is  odd", num);}
+    return 0;}
+
